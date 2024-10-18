f5a74d08eb68787e968bc1859712d9fdd6aab6ca
davidelng <davidelngyt@gmail.com>
2024-10-18 02:32:44 +0200

    infinite loop aggregator

--- handler_agg.go
+++ handler_agg.go
@@ -2,14 +2,57 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/davidelng/gator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <time_between_reqs>", cmd.Name)
+	}
+
+	timeBetweenReqs, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't parse time between reqs: %w", err)
+	}
+
+	log.Printf("Collecting feeds every %s...", timeBetweenReqs)
+
+	ticker := time.NewTicker(timeBetweenReqs)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't find next feed to fetch: %w", err)
+	}
+
+	err = s.db.MarkFeedFetched(context.Background(), database.MarkFeedFetchedParams{
+		LastFetchedAt: sql.NullTime{Time: time.Now().UTC(), Valid: true},
+		UpdatedAt:     time.Now().UTC(),
+		ID:            nextFeed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't update feed: %w", err)
+	}
+
+	fetched, err := fetchFeed(context.Background(), nextFeed.Url)
 	if err != nil {
 		return fmt.Errorf("couldn't fetch feed: %w", err)
 	}
-	fmt.Println(feed)
+
+	for _, item := range fetched.Channel.Item {
+		fmt.Printf("* %s\n", item.Title)
+	}
+
+	log.Printf("Feed %s collected, %v posts found", fetched.Channel.Title, len(fetched.Channel.Item))
+
 	return nil
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -11,3 +11,13 @@ SELECT * FROM feeds WHERE url = $1;
 
 -- name: DeleteFeeds :exec
 DELETE FROM feeds;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = $1, updated_at = $2
+WHERE id = $3;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
